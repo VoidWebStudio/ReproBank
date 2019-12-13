85f593b19485be291910d4651374faca4f34d230
Fuad Hajiyev <hajiyev1996@gmail.com>
2019-12-13 14:16:16 +0400

    responsive

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,11 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var connect = require('gulp-connect');
+<<<<<<< HEAD
 // var pug = require("gulp-pug");
+=======
+var pug = require("gulp-pug");
+>>>>>>> 994641b0b6c61ca54ecbae6d02ad673ad387a4d0
 
 gulp.task('connect', function(){
   connect.server({
@@ -31,6 +35,7 @@ gulp.task('livereload', function (){
 gulp.task('watch', function () {
   gulp.watch('./scss/**/*.scss', ['sass' , 'livereload']);
   gulp.watch('', ['livereload']);
+<<<<<<< HEAD
   // gulp.watch('./modules/**/*.pug', ['pug','livereload']);
 });
 // gulp.task('pug', function(){
@@ -42,3 +47,16 @@ gulp.task('watch', function () {
 // })
 
 gulp.task('default', ['connect', 'watch', 'sass']);
+=======
+  gulp.watch('./modules/**/*.pug', ['pug','livereload']);
+});
+gulp.task('pug', function(){
+  return gulp.src('./App/**/*.pug')
+  .pipe(pug({
+    pretty:true
+  }))
+  .pipe(gulp.dest('./App/'));
+})
+
+gulp.task('default', ['connect', 'pug', 'watch', 'sass']);
+>>>>>>> 994641b0b6c61ca54ecbae6d02ad673ad387a4d0
